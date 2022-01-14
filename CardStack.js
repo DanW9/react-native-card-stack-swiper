bcd9d43de284d235a568f6d1c350071296198e84
Dan Woodward <dan.woodward@ratedpeople.com>
2022-01-14 14:01:47 +0000

    Added spacing prop and fixed zoom on stacked cards

--- CardStack.js
+++ CardStack.js
@@ -23,12 +23,14 @@ class CardStack extends Component {
       drag: new Animated.ValueXY({ x: 0, y: 0 }),
       dragDistance: new Animated.Value(0),
       sindex: 0, // index to the next card to be renderd mod card.length
+      spacing: props.spacing || 15,
       cardA: null,
       cardB: null,
       cardC: null,
       cardD: null,
       cardE: null,
       topCard: "cardA",
+      position: new Animated.Value(-props.spacing || -15),
       cards: [],
       touchStart: 0,
     };
@@ -352,6 +354,13 @@ class CardStack extends Component {
           duration: this.props.duration,
           useNativeDriver: this.props.useNativeDriver || false,
         }).start();
+
+        Animated.spring(this.state.position, {
+          toValue:
+            -this.state.spacing - (this.state.spacing / 2) * previousCardIndex,
+          duration: this.props.duration,
+          useNativeDriver: this.props.useNativeDriver || false,
+        }).start();
       }
     );
   }
@@ -390,6 +399,13 @@ class CardStack extends Component {
       Animated.spring(this.state.dragDistance, {
         toValue: 220,
         duration,
+        useNativeDriver: this.props.useNativeDriver || false,
+      }).start();
+
+      Animated.spring(this.state.position, {
+        toValue:
+          -this.state.spacing - (this.state.spacing / 2) * (nextCard - 1),
+        duration: this.props.duration,
         useNativeDriver: this.props.useNativeDriver || false,
       }).start();
 
@@ -478,6 +494,7 @@ class CardStack extends Component {
 
   render() {
     const { secondCardZoom, renderNoMoreCards } = this.props;
+    const scaleMod = 1 - secondCardZoom;
     const {
       drag,
       dragDistance,
@@ -487,12 +504,66 @@ class CardStack extends Component {
       cardD,
       cardE,
       topCard,
+      spacing,
+      position,
       sindex,
     } = this.state;
 
     const scale = dragDistance.interpolate({
       inputRange: [0, 10, 220],
       outputRange: [secondCardZoom, secondCardZoom, 1],
+      extrapolate: "clamp",
+    });
+    const scale2 = dragDistance.interpolate({
+      inputRange: [0, 10, 220],
+      outputRange: [
+        secondCardZoom - scaleMod,
+        secondCardZoom - scaleMod,
+        secondCardZoom,
+      ],
+      extrapolate: "clamp",
+    });
+    const scale3 = dragDistance.interpolate({
+      inputRange: [0, 10, 220],
+      outputRange: [
+        secondCardZoom - 2 * scaleMod,
+        secondCardZoom - 2 * scaleMod,
+        secondCardZoom - scaleMod,
+      ],
+      extrapolate: "clamp",
+    });
+    const scale4 = dragDistance.interpolate({
+      inputRange: [0, 10, 220],
+      outputRange: [
+        secondCardZoom - 3 * scaleMod,
+        secondCardZoom - 3 * scaleMod,
+        secondCardZoom - 2 * scaleMod,
+      ],
+      extrapolate: "clamp",
+    });
+    const slide = dragDistance.interpolate({
+      inputRange: [0, 220],
+      outputRange: [spacing * 4, spacing * 5],
+      extrapolate: "clamp",
+    });
+    const slide1 = dragDistance.interpolate({
+      inputRange: [0, 220],
+      outputRange: [spacing * 3, spacing * 4],
+      extrapolate: "clamp",
+    });
+    const slide2 = dragDistance.interpolate({
+      inputRange: [0, 220],
+      outputRange: [spacing * 2, spacing * 3],
+      extrapolate: "clamp",
+    });
+    const slide3 = dragDistance.interpolate({
+      inputRange: [0, 220],
+      outputRange: [spacing, spacing * 2],
+      extrapolate: "clamp",
+    });
+    const slide4 = dragDistance.interpolate({
+      inputRange: [0, 220],
+      outputRange: [0, spacing],
       extrapolate: "clamp",
     });
     const rotate = drag.x.interpolate({
@@ -506,121 +577,133 @@ class CardStack extends Component {
         {...this._panResponder.panHandlers}
         style={[{ position: "relative" }, this.props.style]}
       >
-        {renderNoMoreCards()}
         <Animated.View
-          {...this._setPointerEvents(topCard, "cardE")}
           style={[
+            this.props.style,
             {
-              position: "absolute",
-              zIndex: 2,
-              ...Platform.select({
-                android: {
-                  elevation: 2,
-                },
-              }),
-              transform: [
-                { rotate: "0deg" },
-                { translateX: 0 },
-                { translateY: 0 },
-                { scale: scale },
-              ],
+              position: "relative",
+              backgroundColor: "transparent",
+              transform: [{ translateY: position }],
             },
-            this.props.cardContainerStyle,
           ]}
         >
-          {cardE}
-        </Animated.View>
-        <Animated.View
-          {...this._setPointerEvents(topCard, "cardD")}
-          style={[
-            {
-              position: "absolute",
-              zIndex: 2,
-              ...Platform.select({
-                android: {
-                  elevation: 2,
-                },
-              }),
-              transform: [
-                { rotate: "0deg" },
-                { translateX: 0 },
-                { translateY: 0 },
-                { scale: scale },
-              ],
-            },
-            this.props.cardContainerStyle,
-          ]}
-        >
-          {cardD}
-        </Animated.View>
-        <Animated.View
-          {...this._setPointerEvents(topCard, "cardC")}
-          style={[
-            {
-              position: "absolute",
-              zIndex: 2,
-              ...Platform.select({
-                android: {
-                  elevation: 2,
-                },
-              }),
-              transform: [
-                { rotate: "0deg" },
-                { translateX: 0 },
-                { translateY: 0 },
-                { scale: scale },
-              ],
-            },
-            this.props.cardContainerStyle,
-          ]}
-        >
-          {cardC}
-        </Animated.View>
-        <Animated.View
-          {...this._setPointerEvents(topCard, "cardB")}
-          style={[
-            {
-              position: "absolute",
-              zIndex: topCard === "cardB" ? 3 : 2,
-              ...Platform.select({
-                android: {
-                  elevation: topCard === "cardB" ? 3 : 2,
-                },
-              }),
-              transform: [
-                { rotate: topCard === "cardB" ? rotate : "0deg" },
-                { translateX: topCard === "cardB" ? drag.x : 0 },
-                { translateY: topCard === "cardB" ? drag.y : 0 },
-                { scale: topCard === "cardB" ? 1 : scale },
-              ],
-            },
-            this.props.cardContainerStyle,
-          ]}
-        >
-          {cardB}
-        </Animated.View>
-        <Animated.View
-          {...this._setPointerEvents(topCard, "cardA")}
-          style={[
-            {
-              position: "absolute",
-              zIndex: topCard === "cardA" ? 3 : 2,
-              ...Platform.select({
-                android: {
-                  elevation: topCard === "cardA" ? 3 : 2,
-                },
-              }),
-              transform: [
-                { rotate: topCard === "cardA" ? rotate : "0deg" },
-                { translateX: topCard === "cardA" ? drag.x : 0 },
-                { translateY: topCard === "cardA" ? drag.y : 0 },
-                { scale: topCard === "cardA" ? 1 : scale },
-              ],
-            },
-            this.props.cardContainerStyle,
-          ]}
-        >
-          {cardA}
+          {renderNoMoreCards()}
+
+          <Animated.View
+            {...this._setPointerEvents(topCard, "cardE")}
+            style={[
+              {
+                position: "absolute",
+                zIndex: 2,
+                ...Platform.select({
+                  android: {
+                    elevation: 2,
+                  },
+                }),
+                transform: [
+                  { rotate: "0deg" },
+                  { translateX: 0 },
+                  { translateY: slide4 },
+                  { scale: scale4 },
+                ],
+              },
+              this.props.cardContainerStyle,
+            ]}
+          >
+            {cardE}
+          </Animated.View>
+          <Animated.View
+            {...this._setPointerEvents(topCard, "cardD")}
+            style={[
+              {
+                position: "absolute",
+                zIndex: 2,
+                ...Platform.select({
+                  android: {
+                    elevation: 2,
+                  },
+                }),
+                transform: [
+                  { rotate: "0deg" },
+                  { translateX: 0 },
+                  { translateY: slide3 },
+                  { scale: scale3 },
+                ],
+              },
+              this.props.cardContainerStyle,
+            ]}
+          >
+            {cardD}
+          </Animated.View>
+          <Animated.View
+            {...this._setPointerEvents(topCard, "cardC")}
+            style={[
+              {
+                position: "absolute",
+                zIndex: 2,
+                ...Platform.select({
+                  android: {
+                    elevation: 2,
+                  },
+                }),
+                transform: [
+                  { rotate: "0deg" },
+                  { translateX: 0 },
+                  { translateY: slide2 },
+                  { scale: scale2 },
+                ],
+              },
+              this.props.cardContainerStyle,
+            ]}
+          >
+            {cardC}
+          </Animated.View>
+          <Animated.View
+            {...this._setPointerEvents(topCard, "cardB")}
+            style={[
+              {
+                position: "absolute",
+                zIndex: topCard === "cardB" ? 3 : 2,
+                ...Platform.select({
+                  android: {
+                    elevation: topCard === "cardB" ? 3 : 2,
+                  },
+                }),
+                transform: [
+                  { rotate: topCard === "cardB" ? rotate : "0deg" },
+                  { translateX: topCard === "cardB" ? drag.x : 0 },
+                  { translateY: topCard === "cardB" ? slide : slide1 },
+                  { scale: topCard === "cardB" ? 1 : scale },
+                ],
+              },
+              this.props.cardContainerStyle,
+            ]}
+          >
+            {cardB}
+          </Animated.View>
+          <Animated.View
+            {...this._setPointerEvents(topCard, "cardA")}
+            style={[
+              {
+                position: "absolute",
+                zIndex: topCard === "cardA" ? 3 : 2,
+                ...Platform.select({
+                  android: {
+                    elevation: topCard === "cardA" ? 3 : 2,
+                  },
+                }),
+                transform: [
+                  { rotate: topCard === "cardA" ? rotate : "0deg" },
+                  { translateX: topCard === "cardA" ? drag.x : 0 },
+                  { translateY: topCard === "cardA" ? slide : slide1 },
+                  { scale: topCard === "cardA" ? 1 : scale },
+                ],
+              },
+              this.props.cardContainerStyle,
+            ]}
+          >
+            {cardA}
+          </Animated.View>
         </Animated.View>
       </View>
     );
@@ -629,7 +712,7 @@ class CardStack extends Component {
 
 CardStack.propTypes = {
   children: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
-
+  spacing: PropTypes.number,
   style: PropTypes.oneOfType([
     PropTypes.number,
     PropTypes.object,
@@ -671,6 +754,7 @@ CardStack.propTypes = {
 };
 
 CardStack.defaultProps = {
+  spacing: 15,
   style: {},
   cardContainerStyle: {},
   secondCardZoom: 1,
